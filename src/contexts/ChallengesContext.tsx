f8fc6455a085d83c6a50826ef87478e8b917bc08
Cyntia Santos <cyntia.aparecida@hotmail.com>
2023-09-22 16:08:38 -0300

    changed timer

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, useState, ReactNode, useEffect } from 'react';
-import Cookies from 'js-cookie';
+import { createContext, useState, ReactNode, useEffect } from "react";
+import Cookies from "js-cookie";
 import challenges from '../../challenges.json';
 import { LevelUpModal } from '../components/LevelUpModal';
 
